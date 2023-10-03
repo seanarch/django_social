048d2c3a007e9722ca61ffe29f2433b265944821
seanarch <zhuqingyun@outlook.com>
2023-10-03 13:58:47 -0600

    Sign up new user profile

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
 
 # Create your views here.
 def index(request): 
@@ -24,6 +25,14 @@ def signup(request):
             else: 
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
+
+                #Log user in and redirect to settings page 
+
+                #Create a profile object for the new user 
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save() 
+                return redirect('signup')
         else: 
             messages.info(request, 'Password Not Matching')
             return redirect('signup')         
