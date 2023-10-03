e4a12cb7d63ffd565dc565a5b7d7763f60450dd4
seanarch <zhuqingyun@outlook.com>
2023-10-03 14:10:52 -0600

    modify templates of signin and signup

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('signup', views.signup, name="singup")
+    path('signup', views.signup, name="signup"),
+    path('signin', views.signin, name="signin"),
 ]
--- social_book/core/views.py
+++ social_book/core/views.py
@@ -38,3 +38,6 @@ def signup(request):
             return redirect('signup')         
     else: 
         return render(request, 'signup.html')
+    
+def signin(request): 
+    return render(request, 'signin.html')
