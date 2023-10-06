3a4627e450bdaab63ef4c2693ae020b083aaaa87
seanarch <zhuqingyun@outlook.com>
2023-10-06 11:57:00 -0600

    modify index

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -9,7 +9,9 @@ from .models import Profile
 
 @login_required(login_url='signin')
 def index(request): 
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object) 
+    return render(request, 'index.html', {'user_profile': user_profile})
 
 def upload(request):
     return HttpResponse('<h1>Upload View</h1>')
